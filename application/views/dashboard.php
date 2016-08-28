3ebe974e0c45a3ab6f769e223277ffb0642831bc
bibiyo21 <bibiyo.sotejo@gmail.com>
2016-08-28 13:25:24 +0800

    update
    update

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -59,7 +59,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Donation</h4>
+            <h4 class="modal-title">Volunteer</h4>
           </div>
           <div class="modal-body">
   <form class="form-horizontal" action="<?php echo base_url();?>index.php/home/home2" method="POST">
@@ -98,50 +98,3 @@
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
-<div class="modal fade" tabindex="-1" role="dialog" id="volunteer_modal">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Modal title</h4>
-          </div>
-          <div class="modal-body">
-	<h1>Donation</h1>
-	<form class="form-horizontal" action="<?php echo base_url();?>index.php/home/home2" method="POST">
-	  	<div class="form-group">
-	    	<label for="inputPassword3" class="col-sm-2 control-label">Account Number</label>
-	    	<div class="col-sm-10">
-	      		<input type="text" class="form-control" name="source_account" placeholder="Account Number" required="">
-	    	</div>
-	  	</div>
-	  	<div class="form-group">
-	    	<label for="inputPassword3" class="col-sm-2 control-label">Currency Code</label>
-	    	<div class="col-sm-10">
-	      		<select class="form-control" name="source_currency">
-	      			<option value="php" selected>PHP</option>
-	      			<!-- <option value="usd">USD</option>
-	      			<option value="aud">AUD</option>
-	      			<option value="eur">EUR</option> -->
-	      		</select>
-	    	</div>
-	  	</div>
-	  	<div class="form-group">
-	    	<label for="inputPassword3" class="col-sm-2 control-label">Amount</label>
-	    	<div class="col-sm-10">
-	      		<input type="number" class="form-control" id="inputPassword3" name="amount" min="1.00" placeholder="Amount to Donate" required="required">
-	    	</div>
-	  	</div>
-	  	<div class="form-group">
-		    <div class="col-sm-offset-2 col-sm-10">
-		      <button type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-heart"></i> Donate</button>
-		    </div>
-	  	</div>
-	</form>
-          </div>
-          <div class="modal-footer">
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-
-	
